06b6a16c3a7bcf0e25399e16baa2230f925816d2
Mukesh Raja J <Mukeshcse31@gmail.com>
2019-07-11 22:06:17 -0600

    GEC server set up

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.google.app.javajokes.Jokes;
 
 import javax.inject.Named;
 
@@ -22,7 +23,9 @@ public class MyEndpoint {
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        Jokes jokes = new Jokes();
+        response.setData(jokes.getJokes());
+//        response.setData("Hi, " + name);
 
         return response;
     }
